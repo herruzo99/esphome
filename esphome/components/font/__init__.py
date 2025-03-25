a587ef0f36c112c1c9e574523c0a5f37bc36b20e
Samuel Sieb <samuel-github@sieb.net>
2025-03-25 01:33:53 -0700

    fix 1bpp rendering (#8463)

--- esphome/components/font/__init__.py
+++ esphome/components/font/__init__.py
@@ -12,8 +12,8 @@ import esphome_glyphsets as glyphsets
 from freetype import (
     FT_LOAD_NO_BITMAP,
     FT_LOAD_RENDER,
+    FT_LOAD_TARGET_MONO,
     Face,
-    ft_pixel_mode_grays,
     ft_pixel_mode_mono,
 )
 import requests
@@ -524,6 +524,7 @@ async def to_code(config):
         if bpp != 1:
             flags |= FT_LOAD_NO_BITMAP
 <<<<<<< HEAD
+<<<<<<< HEAD
         else:
             flags |= FT_LOAD_TARGET_MONO
         font.load_char(codepoint, flags)
@@ -531,6 +532,11 @@ async def to_code(config):
         font.load_char(codepoint, flags)
         font.glyph.render(mode)
 >>>>>>> 20c777852 ([font] More robust handling of fixed font sizes. (#8443))
+=======
+        else:
+            flags |= FT_LOAD_TARGET_MONO
+        font.load_char(codepoint, flags)
+>>>>>>> f131186e6 (fix 1bpp rendering (#8463))
         width = font.glyph.bitmap.width
         height = font.glyph.bitmap.rows
         buffer = font.glyph.bitmap.buffer
